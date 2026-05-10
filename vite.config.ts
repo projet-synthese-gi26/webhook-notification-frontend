680fbaf76eac1a0da4ae0852a8a1479be79a654a
JonaBacho <110006848+JonaBacho@users.noreply.github.com>
2026-05-10 22:17:55 +0100

    Update Vite config with server and preview settings
    Added server and preview configuration with allowed hosts.

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,22 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
-/*
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+
   server: {
-    port: 3000, // ← choisis le port que tu veux
-    strictPort: true, // ← évite que Vite change automatiquement
+    host: '0.0.0.0',
+    port: 3000,
+    strictPort: true,
+    allowedHosts: [
+      'webhook-dev.yowyob.com'
+    ]
   },
-});
-*/
+
+  preview: {
+    allowedHosts: [
+      'webhook-dev.yowyob.com'
+    ]
+  }
+})
